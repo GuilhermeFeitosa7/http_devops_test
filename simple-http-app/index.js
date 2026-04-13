9441b3d6c4c53d856de7f5e34088adb17049da11
Guilherme Feitosa <guilherme.feitosa7.com>
2026-04-13 02:17:38 -0300

    Atualizando .js

--- simple-http-app/index.js
+++ simple-http-app/index.js
@@ -4,9 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-  res.status(200).send("Aplicação rodando com sucesso!");
+  res.status(200).send(`<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Simple HTTP App</title>
+  </head>
+  <body>
+    <h1>Aplicação rodando com sucesso!</h1>
+  </body>
+</html>`);
 });
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor escutando em http://0.0.0.0:${PORT}`);
 });
